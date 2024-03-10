190a47152e3acf17fd76950cc075a274d2d0b23c
Diego Macario <diego.macario5@gmail.com>
2024-03-09 19:06:50 -0500

    Do a few experiments related to single pixel drawing.

--- examples/Tiny_Ray_Tracer/main.cpp
+++ examples/Tiny_Ray_Tracer/main.cpp
@@ -36,25 +36,46 @@ void setup()
 void loop()
 {
     epd_poweron();
-    epd_draw_hline(10, random(10, EPD_HEIGHT), EPD_WIDTH - 20, 0, framebuffer);
-    epd_draw_grayscale_image(epd_full_screen(), framebuffer);
-    delay(1000);
 
-    epd_draw_rect(10, random(10, EPD_HEIGHT), random(10, 60), random(10, 120), 0, framebuffer);
-    epd_draw_grayscale_image(epd_full_screen(), framebuffer);
-    delay(1000);
+    // epd_draw_hline(10, random(10, EPD_HEIGHT), EPD_WIDTH - 20, 0, framebuffer);
+    // epd_draw_grayscale_image(epd_full_screen(), framebuffer);
+    // delay(1000);
 
-    epd_draw_circle(random(10, EPD_WIDTH), random(10, EPD_HEIGHT), 120, 0, framebuffer);
-    epd_draw_grayscale_image(epd_full_screen(), framebuffer);
-    delay(1000);
+    // epd_draw_rect(10, random(10, EPD_HEIGHT), random(10, 60), random(10, 120), 0, framebuffer);
+    // epd_draw_grayscale_image(epd_full_screen(), framebuffer);
+    // delay(1000);
 
-    epd_fill_rect(10, random(10, EPD_HEIGHT), random(10, 60), random(10, 120), 0, framebuffer);
-    epd_draw_grayscale_image(epd_full_screen(), framebuffer);
-    delay(1000);
+    // epd_draw_circle(random(10, EPD_WIDTH), random(10, EPD_HEIGHT), 120, 0, framebuffer);
+    // epd_draw_grayscale_image(epd_full_screen(), framebuffer);
+    // delay(1000);
 
-    epd_fill_circle(random(10, EPD_WIDTH), random(10, EPD_HEIGHT), random(10, 160), 0, framebuffer);
+    // epd_fill_rect(10, random(10, EPD_HEIGHT), random(10, 60), random(10, 120), 0, framebuffer);
+    // epd_draw_grayscale_image(epd_full_screen(), framebuffer);
+    // delay(1000);
+
+    // epd_fill_circle(random(10, EPD_WIDTH), random(10, EPD_HEIGHT), random(10, 160), 0, framebuffer);
+    // epd_draw_grayscale_image(epd_full_screen(), framebuffer);
+    // delay(1000);
+
+    // ---
+
+    // epd_fill_rect(0, 0, EPD_WIDTH * 0.9f, EPD_HEIGHT * 0.9f, 0, framebuffer);
+    // epd_draw_grayscale_image(epd_full_screen(), framebuffer);
+    // delay(5000);
+
+    // for (int i = 0; i < EPD_WIDTH; ++i) {
+    //     epd_fill_rect(random(0, EPD_WIDTH), random(0, EPD_HEIGHT), 1, 1, 0/*random(0, 255)*/, framebuffer);
+    // }
+    // epd_draw_grayscale_image(epd_full_screen(), framebuffer);
+    // delay(5000);
+
+    for (int32_t i = 0; i < EPD_WIDTH; ++i) {
+        for (int32_t j = 0; j < EPD_HEIGHT; ++j) {
+            epd_fill_rect(i, j, 1, 1, 0, framebuffer);
+        }
+    }
     epd_draw_grayscale_image(epd_full_screen(), framebuffer);
-    delay(1000);
+    delay(5000);
 
     memset(framebuffer, 0xFF, EPD_WIDTH * EPD_HEIGHT / 2);
     epd_clear();
