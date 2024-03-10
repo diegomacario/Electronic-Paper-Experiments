cfd09c576cccb28858b62d99e0ba4e1cb27d8f2f
Diego Macario <diego.macario5@gmail.com>
2024-03-10 11:27:07 -0400

    Figure out how to draw individual pixels.

--- examples/Tiny_Ray_Tracer/main.cpp
+++ examples/Tiny_Ray_Tracer/main.cpp
@@ -16,7 +16,7 @@ void setup()
 {
     Serial.begin(115200);
     delay(1000);
-    framebuffer = (uint8_t *)ps_calloc(sizeof(uint8_t), EPD_WIDTH * EPD_HEIGHT / 2);
+    framebuffer = (uint8_t *)heap_caps_malloc(EPD_WIDTH * EPD_HEIGHT / 2, MALLOC_CAP_SPIRAM);
     if (!framebuffer) {
         Serial.println("Memory allocation failed!");
         while (1);
@@ -69,9 +69,10 @@ void loop()
     // epd_draw_grayscale_image(epd_full_screen(), framebuffer);
     // delay(5000);
 
-    for (int32_t i = 0; i < EPD_WIDTH; ++i) {
-        for (int32_t j = 0; j < EPD_HEIGHT; ++j) {
-            epd_fill_rect(i, j, 1, 1, 0, framebuffer);
+    for (int32_t i = 0; i < 240; ++i) {
+        for (int32_t j = 0; j < 135; ++j) {
+            uint8_t value = random(255);
+            epd_draw_pixel(i, j, value, framebuffer);
         }
     }
     epd_draw_grayscale_image(epd_full_screen(), framebuffer);
